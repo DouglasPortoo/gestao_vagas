bdb1fa09468ba5e159687493f6c1f2ef76f3538a
Douglas <portogdouglas@gmail.com>
2023-10-25 10:29:26 -0300

    Formatando mensagem de validação

--- src/main/java/br/com/douglasporto/gestao_vagas/modules/candidate/CandidateEntity.java
+++ src/main/java/br/com/douglasporto/gestao_vagas/modules/candidate/CandidateEntity.java
@@ -14,13 +14,13 @@ public class CandidateEntity {
   private UUID id;
   private String name;
 
-  @Pattern(regexp = "^(?!\\s*$).+", message = "O campo não pode ter espaços em branco")
+  @Pattern(regexp = "\\S+", message = "O campo não pode ter espaços em branco")
   private String username;
 
   @Email(message ="O campo deve conter um e-mail válido")
   private String email;
 
-  @Length( min = 5, max = 20)
+  @Length( min = 5, max = 20, message = "A senha deve ter no minimo 5 caracteres e no maxio 20 caracteres")
   private String password;
   private String description;
   private String curriculum;
